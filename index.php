dc33e11f6987b1eff1b7a4b305ec85ff1201ca0c
njourou <muirurishem@gmail.com>
2023-01-10 13:21:18 +0300

    added our tenants

--- index.php
+++ index.php
@@ -66,6 +66,172 @@ include "./init/includes/header.php";
     <main id="main">
 
 
+<!--Our Services-->
+
+<!--End Our Services-->
+
+    <div class="section mt-3">
+            <div class="px-3">
+                <div class="theme-container">
+                    <h1 class="section-title mb-3">Our Tenants</h1>
+                   
+                    <div class="clients-carousel"> 
+                        <div class="swiper-container"> 
+                            <div class="swiper-wrapper">      
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/img/tenants/cropped-CPF-Financial-Services-1.jpeg" alt="slide image" data-src="assets/img/tenants/cropped-CPF-Financial-Services-1.jpeg" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/ecp-logo2.png" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/Emirates-Logo.png" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide"> 
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/Fragomen-Worldwide.png" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/image.png" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/logo-bollore-logistics-color.png" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/slusol-logo.jpg" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="client-item"> 
+                                        <img src="assets/images/others/transparent-bg.png" alt="slide image" data-src="assets/img/tenants/TLScontact_main.svg" class="swiper-lazy"> 
+                                        <div class="swiper-lazy-preloader"></div>
+                                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                    </div> 
+                </div>
+            </div>   
+        </div> 
+
+
+        <!--Contact Us-->
+        
+        
+        <div class="px-3">  
+            <div class="theme-container"> 
+                <div class="mdc-card main-content-header mb-5"> 
+                    <div class="row around-xs">
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="column center-xs middle-xs text-center">
+                                <i class="material-icons mat-icon-lg primary-color">home</i>
+                                <h3 class="primary-color py-1">ADDRESS :</h3>
+                                <span class="text-muted fw-500">2903 Avenue Z, Brooklyn, New York City</span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="column center-xs middle-xs text-center">
+                                <i class="material-icons mat-icon-lg primary-color">call</i>
+                                <h3 class="primary-color py-1">PHONES :</h3>
+                                <span class="text-muted fw-500">(212) 457-2308</span>
+                                <span class="text-muted fw-500">(212) 457-2309</span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="column center-xs middle-xs text-center">
+                                <i class="material-icons mat-icon-lg primary-color">mail_outline</i>
+                                <h3 class="primary-color py-1">E-MAIL :</h3>
+                                <span class="text-muted fw-500">contact@housekey.com</span>
+                                <span class="text-muted fw-500">info@housekey.com</span>
+                            </div>
+                        </div> 
+                        <div class="col-xs-12 mt-3 px-3 p-relative">
+                            <div class="divider w-100"></div>
+                        </div> 
+                        <h3 class="w-100 text-center py-3">CONTACT US</h3> 
+                        <form action="javascript:void(0);" class="contact-form row"> 
+                            <div class="col-xs-12 col-sm-12 col-md-4 p-2">  
+                                <div class="mdc-text-field mdc-text-field--outlined w-100">
+                                    <input class="mdc-text-field__input" placeholder="Name is required">
+                                    <div class="mdc-notched-outline">
+                                        <div class="mdc-notched-outline__leading"></div>
+                                        <div class="mdc-notched-outline__notch">
+                                            <label class="mdc-floating-label">Name</label>
+                                        </div>
+                                        <div class="mdc-notched-outline__trailing"></div>
+                                    </div>
+                                </div> 
+                            </div>  
+                            <div class="col-xs-12 col-sm-12 col-md-4 p-2">  
+                                <div class="mdc-text-field mdc-text-field--outlined w-100">
+                                    <input class="mdc-text-field__input" placeholder="Email is required">
+                                    <div class="mdc-notched-outline">
+                                        <div class="mdc-notched-outline__leading"></div>
+                                        <div class="mdc-notched-outline__notch">
+                                            <label class="mdc-floating-label">Email</label>
+                                        </div>
+                                        <div class="mdc-notched-outline__trailing"></div>
+                                    </div>
+                                </div> 
+                            </div>  
+                            <div class="col-xs-12 col-sm-12 col-md-4 p-2">  
+                                <div class="mdc-text-field mdc-text-field--outlined w-100">
+                                    <input class="mdc-text-field__input" placeholder="Phone is required">
+                                    <div class="mdc-notched-outline">
+                                        <div class="mdc-notched-outline__leading"></div>
+                                        <div class="mdc-notched-outline__notch">
+                                            <label class="mdc-floating-label">Phone</label>
+                                        </div>
+                                        <div class="mdc-notched-outline__trailing"></div>
+                                    </div>
+                                </div> 
+                            </div>  
+                            <div class="col-xs-12 p-2">  
+                                <div class="mdc-text-field mdc-text-field--outlined mdc-text-field--textarea w-100">
+                                    <textarea class="mdc-text-field__input" rows="5" placeholder="Message is required"></textarea>
+                                    <div class="mdc-notched-outline mdc-notched-outline--upgraded">
+                                        <div class="mdc-notched-outline__leading"></div>
+                                        <div class="mdc-notched-outline__notch">
+                                            <label class="mdc-floating-label">Message</label>
+                                        </div>
+                                        <div class="mdc-notched-outline__trailing"></div>
+                                    </div>
+                                </div>
+                            </div>   
+                            <div class="col-xs-12 w-100 py-3 text-center">
+                                <button class="mdc-button mdc-button--raised" type="submit">
+                                    <span class="mdc-button__ripple"></span> 
+                                    <span class="mdc-button__label">Submit</span> 
+                                </button>   
+                            </div> 
+                        </form>  
+                    </div> 
+                    <div class="mt-5">
+                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d997.2127724530903!2d36.80340762598876!3d-1.2616382359540153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f172ee7a89561%3A0x4dc6ffb4a31f25a0!2sKenroid%20Group!5e0!3m2!1sen!2ske!4v1670493544107!5m2!1sen!2ske" width="1250" height="250" style="border: 4px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <!-- <div id="contact-map"></div> -->
+                    </div> 
+                </div>
+            </div>  
+        </div>  
 
     </main>
     <?php
